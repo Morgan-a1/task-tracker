a6cef8a1b9eab723b4619ab3fb8c1315fe872efa
Morgan <morgan_a1@hotmail.com>
2021-12-09 15:15:22 +0100

    Add toggle reminder functionality

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import Task from "./Task"
 
-function Tasks({ tasks }) {
+function Tasks({ tasks, onToggle }) {
    if (!tasks || tasks.length === 0) {
       return <p>No tasks to display</p>;
    }
    
-   return tasks.map((task) => <Task key={task.id} task={task} />);
+   return tasks.map((task) => <Task onToggle={onToggle} key={task.id} task={task} />);
 }
 
 Tasks.defaultProps = {
@@ -15,6 +15,7 @@ Tasks.defaultProps = {
 
 Tasks.propTupes = {
    tasks: PropTypes.array,
+   onToggle: PropTypes.func.isRequired,
 };
 
 export default Tasks;
